d594cc7ad87f436f4bded216c04cd8bc2553dcea
myriagkos <filipposmyriagkos@gmail.com>
2026-05-15 08:25:09 +0300

    Add files via upload

---
+++ Button-Loading/app.js
@@ -0,0 +1,17 @@
+const loadButton = document.getElementById('loadButton');
+const loader = document.getElementById('loader');
+const demoForm = document.getElementById('my-form');
+
+loadButton.addEventListener('click', () => {
+    //disable the button and prevent further clicks 
+    loadButton.disabled = true;
+    loader.style.display= 'inline-block';
+
+    setTimeout(() => {
+        //restore the button state after the operation is done 
+        loadButton.disabled = false;
+        loader.style.display= 'none';
+        demoForm.reset();
+
+    }, 2000);
+});
